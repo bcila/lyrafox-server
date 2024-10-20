0780904799b7e28689703617bdc140c29b08879e
bcila <b.cila51@gmail.com>
2024-10-20 21:09:22 +0300

    pre-commit hook setup

--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { data } from '../data/reviews';
-
-@Injectable()
-export class AppService {
-  getHello() {
-    return data.reviews[0];
-  }
-}
