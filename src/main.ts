7279bcbcc4384425ce9f01d40ca5d85a4d4f8ca4
bcila <b.cila51@gmail.com>
2024-10-20 18:12:45 +0300

    swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
     // allowedHeaders: 'Content-Type, Authorization',
     // credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('LyraFox')
+    .setDescription('The LyraFox API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
