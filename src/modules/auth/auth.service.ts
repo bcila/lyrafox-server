5e43c7f54bee60f07312fabd6de3fea82a1fa76a
bcila <b.cila51@gmail.com>
2024-10-30 23:34:03 +0300

    fix: double quote to single

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { UsersService } from '../users/users.service';
 import * as argon2 from 'argon2';
 import { JwtService } from '@nestjs/jwt';
 import { RegisterDto } from './dto/register.dto';
-import { UserWithoutPassword } from "../users/types/users.types";
+import { UserWithoutPassword } from '../users/types/users.types';
 
 @Injectable()
 export class AuthService {
